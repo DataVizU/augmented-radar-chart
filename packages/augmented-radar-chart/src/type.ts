333e64fa748802a3ad5b6a6ca782abd2065df898
AddInistrator <2812221043@qq.com>
2024-11-25 15:25:49 +0800

    feat: update params

--- packages/augmented-radar-chart/src/type.ts
+++ packages/augmented-radar-chart/src/type.ts
@@ -7,94 +7,31 @@ export type AugmentedRadarChartData<
 > = FixedLengthArray<Record<K, number>, N>;
 
 export interface AugmentedRadarChartOptions {
-  // container of the chart
+  // DOM element to render the chart
   container: HTMLElement;
-
-  // size of the chart
+  // The size of the chart
   size: number;
 
-  data: AugmentedRadarChartData;
-
-  // name and max value for each dimension
-  // in case dimension names are not correspond with the keys of data
-  // TODO: both radar chart and horizon chart apply the same linear scale (min to max)?
-  // TODO: support custom scale
-  indicator: Array<{
-    name?: string;
-    min?: number;
-    max?: number;
-  }>;
-
-  startAngle?: number;
-
-  clockwise?: boolean;
-
-  radarChartOptions: {
-    // style of axis
+  // Styling options for the radar chart
+  styles: {
     label: {
-      // color of the label text
-      textColor?: string;
-      // offset of the label
+      // Text color for axis labels
+      color?: string;
+      // Distance offset for labels from axis ends
       offset?: number;
     };
-
-    // style of line
     line: {
       color?: string;
-      lineWidth?: number;
-      lineType?: 'solid' | 'dashed' | 'dotted'; // TODO: other type
-      opacity?: number;
+      width?: number;
+      type?: string;
     };
-
-    dot: {
-      dotRadius?: number;
-      dotType?: 'circle' | 'rect'; // TODO: other type
-    };
-
-    // style of background area
     area: {
-      // color of the background
-      color?: string;
-      opacity?: number;
-      borderColor?: string;
-      borderWidth?: number;
-    };
-  };
-
-  horizonChartOptions: {
-    area: {
-      // number of layers
-      split: number;
-      // color of the uppermost layer
-      color: string | Array<string>;
-    };
-
-    axis: {
-      // number of ticks in each layer
-      lineCount: number;
-      lineColor: string;
-      opacity?: number;
-    };
-
-    label: {
-      // color of the label text
-      textColor?: string;
-      // offset of the label
-      offset?: number;
-    };
-
-    dot: {
-      dotRadius?: number;
-      dotType?: 'circle' | 'rect'; // TODO: other type
+      layerCount: number;
+      colors: string | Array<string>;
     };
   };
 }
 
-// TODO: specific options
-export interface AugmentedRadarChartSVGOptions extends AugmentedRadarChartOptions {
-  behavior?: Array<(event: MouseEvent, data: unknown) => void>;
-}
+export interface AugmentedRadarChartSVGOptions extends AugmentedRadarChartOptions {}
 
-export interface AugmentedRadarChartCanvasOptions extends AugmentedRadarChartOptions {
-  behavior?: Array<(type: 'hover' | 'click', x: number, y: number, data: unknown) => void>;
-}
+export interface AugmentedRadarChartCanvasOptions extends AugmentedRadarChartOptions {}
