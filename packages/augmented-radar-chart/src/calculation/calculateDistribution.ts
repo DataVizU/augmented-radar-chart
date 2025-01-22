29fbc05b4b41b1d34c3a104c65396a6341710d4a
La515 <3980181523@qq.com>
2025-01-22 21:30:02 +0800

    feat: eslint

--- packages/augmented-radar-chart/src/calculation/calculateDistribution.ts
+++ packages/augmented-radar-chart/src/calculation/calculateDistribution.ts
@@ -13,14 +13,14 @@ export function calculateDistribution(
    */
   return Object.keys(data).reduce((acc, key) => {
     const points = data[key];
-    const pointmin = Math.min(...points.map(p => p.point));
-    const pointmax = Math.max(...points.map(p => p.point));
-    const { start , end } = config?.[key] ??{start:pointmin, end:pointmax};                
+    const pointmin = Math.min(...points.map((p) => p.point));
+    const pointmax = Math.max(...points.map((p) => p.point));
+    const { start, end } = config?.[key] ?? { start: pointmin, end: pointmax };
     return {
       ...acc,
       [key]: data[key].map(({ point, value }) => ({
-        point:(point - start)/ (end - start),
-        value: value
+        point: (point - start) / (end - start),
+        value: value,
       })),
     };
   }, {});
